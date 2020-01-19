ce32873bd7bdce71359d21e7b0af16f2d110d6eb
Bushra Al-Mahdi <diamondprincessbushra@gmail.com>
2020-01-19 11:57:31 +0300

    Initial commit

---
+++ app/src/main/java/com/bushra/sunset/SunsetActivity.java
@@ -0,0 +1,13 @@
+package com.bushra.sunset;
+
+import android.support.v4.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity
+{
+
+    @Override
+    protected Fragment createFragment()
+    {
+        return SunsetFragment.newInstance();
+    }
+}
